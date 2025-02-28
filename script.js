ade9a0bc5cad2a5d5f4126e8a417e8383042692a
Junayed Hassan Sojol <49646202+JunayedHassan@users.noreply.github.com>
2025-02-28 21:45:15 +0600

    Add files via upload
    simple website (not mobile responsive)

---
+++ script.js
@@ -0,0 +1,27 @@
+const textElement = document.querySelector(".text-rotate");
+const words = ["Developer", "Operational Manager", "Dreamer"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const currentWord = words[wordIndex];
+  if (isDeleting) {
+    charIndex--;
+  } else {
+    charIndex++;
+  }
+
+  textElement.textContent = currentWord.substring(0, charIndex);
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    setTimeout(() => (isDeleting = true), 1000); // Pause before deleting
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length; // Move to next word
+  }
+
+  setTimeout(typeEffect, isDeleting ? 50 : 100); // Typing speed
+}
+
+typeEffect();
